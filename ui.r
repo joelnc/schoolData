38723e39c3cd52508e99d10128ceac5b093a98f1
joelnc <joel.nipper@charlottenc.gov>
2017-09-18 07:45:21 -0400

    UI changes
    Fairly straightfowrard. Load the shape file.  For funding; several
    district names did not match shapefile names, thus the renaming.  added
    slider

--- ui.r
+++ ui.r
@@ -1,22 +1,26 @@
 library(shiny)
 
 ## Load, format, subset, WQD File
-ds <- read.csv("test2.csv",
-                 stringsAsFactors=FALSE, sep=",", header=TRUE)
-
-ds <<- ds %>%
-    mutate(SalFrac = Sal/(Sal+Ben+Inst),
-           BenFrac = Ben/(Sal+Ben+Inst),
-           InstFrac = Inst/(Sal+Ben+Inst))
-
 ds2 <- read.csv(file="funding.csv", sep=",", stringsAsFactors=FALSE,
                 header=T)
 
 ds2$key <- paste(ds2$Lea_Name, ds2$year)
 dsSlim <- ds2[!duplicated(ds2$key),]
-dsSlim <<- dsSlim[dsSlim$year==2015,]
 
+## Fix names from funding to match names from Tigris
+dsSlim$Lea_Name[dsSlim$Lea_Name=="Edgecombe County Public School"] <-
+    "Edgecombe County Schools"
+dsSlim$Lea_Name[dsSlim$Lea_Name=="Newton Conover City Schools"] <-
+    "Newton-Conover City Schools"
+dsSlim$Lea_Name[dsSlim$Lea_Name=="Lenoir County Public Schools"] <-
+    "Lenoir County Schools"
+dsSlim$Lea_Name[dsSlim$Lea_Name=="Carteret County Public Schools"] <-
+    "Carteret County Schools"
 
+dsSlim <<- dsSlim
+##dsSlim <<- dsSlim[dsSlim$year==2015,]
+
+## Load rds shapefile of school districts
 sds <<- readRDS(file="sds.rds")
 
 
@@ -28,8 +32,10 @@ shinyUI(
                             selectInput("field", "Data to plot:",
                                         names(dsSlim),
                                         selected="lea_total_expense_num"),
-
-                            plotlyOutput("bar2")
+                            plotlyOutput("bar2"),
+                            sliderInput("year", "Select Year",
+                                        min=2002, max=2016, value=2015,
+                                        animate=TRUE)
                             ),
                         fluidRow(
                             plotlyOutput("bar3")
